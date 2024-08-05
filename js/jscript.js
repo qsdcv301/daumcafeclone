06d5d0072be85b07c3abe5581255ca2e07f7b614
qsdcv301 <qsdcv301@naver.com>
2024-08-05 17:34:28 +0900

    section1 완료 section2 진행중

--- js/jscript.js
+++ js/jscript.js
@@ -9,15 +9,7 @@ $(function () {
     });
 
     $(".btn-left").click(function () {
-        i--;
-        if (i < 1) {
-            i = 7;
-        }
-        console.log(i);
-        $(".hero .hero-box:last").clone().prependTo('.hero');
-        $(".hero .hero-box:last").remove();
-        $('.hero .hero-box:eq(0)').addClass('act');
-        $('.hero .hero-box:eq(0) ul>li:first-child a').addClass("active");
+        fadeOutIn();
     });
 
     $(".btn-right").click(function () {
@@ -27,14 +19,16 @@ $(function () {
     setInterval(fadeInOut, 9000);
 
 }); // /.jquery
-
+function fadeOutIn(){
+    let ct = $(".hero-box").length;
+    $(".hero .hero-box:eq("+(ct-1)+")").clone().prependTo('.hero');
+    $(".hero .hero-box:eq("+ct+")").remove();
+    $(".hero .hero-box").removeClass("act");
+    $('.hero .hero-box:eq(0)').addClass('act');
+    $('.hero .hero-box:eq(0) ul>li:first-child a').addClass("active");
+}
 
 function fadeInOut() {
-    i++;
-    if (i > 7) {
-        i = 1;
-    }
-    console.log(i);
     $(".hero .hero-box:eq(0)").clone().appendTo('.hero');
     $(".hero .hero-box:eq(0)").remove();
     $('.hero .hero-box:eq(0)').addClass('act');
