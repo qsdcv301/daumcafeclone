47329f244395ccf3fa2cbab296e3aec3e3fa4370
qsdcv301 <qsdcv301@naver.com>
2024-08-07 11:16:15 +0900

    b-date 부분 디자인 작업완료 및 javascript 진행중

--- js/jscript.js
+++ js/jscript.js
@@ -1,4 +1,5 @@
 let i = 0;
+let count = 0;
 $(function () {
     $(".grid-fill").hover(function () {
         $(this).find(".subnav").stop().fadeToggle();
@@ -16,13 +17,46 @@ $(function () {
         fadeInOut();
     });
 
+    $(".b-table").eq(0).css("display", "flex");
+    fadeInOut();
     setInterval(fadeInOut, 9000);
 
+    const bTableCount = $(".b-table").length;
+    $(document).on("click", ".run", function () {
+        if ($(this).hasClass("btn-next")) {
+            if (count > bTableCount - 2) {
+                $(this).removeClass("run");
+                return;
+            } else {
+                count++;
+                if (count > 0) {
+                    $(".btn-prev").addClass("run");
+                }
+            }
+        } else {
+            if (count < 1) {
+                $(".btn-prev").removeClass("run");
+            } else {
+                count--;
+                if (count < bTableCount - 2) {
+                    $(".btn-next").addClass("run");
+                }
+            }
+        }
+        $(".b-table").hide();
+        $(".b-table").eq(count).css("display", "flex");
+    });
+
+    $(".btn-prev.run").click(function (e) {
+        e.preventDefault();
+        alert("누름");
+    });
+
 }); // /.jquery
-function fadeOutIn(){
+function fadeOutIn() {
     let ct = $(".hero-box").length;
-    $(".hero .hero-box:eq("+(ct-1)+")").clone().prependTo('.hero');
-    $(".hero .hero-box:eq("+ct+")").remove();
+    $(".hero .hero-box:eq(" + (ct - 1) + ")").clone().prependTo('.hero');
+    $(".hero .hero-box:eq(" + ct + ")").remove();
     $(".hero .hero-box").removeClass("act");
     $('.hero .hero-box:eq(0)').addClass('act');
     $('.hero .hero-box:eq(0) ul>li:first-child a').addClass("active");
